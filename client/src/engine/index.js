bc7f93497ad465a0f9653db1a8850a3e24882778
ivanvihe <52743025+ivanvihe@users.noreply.github.com>
2025-10-17 10:11:58 +0200

    feat: add canvas rendering engine

---
+++ client/src/engine/index.js
@@ -0,0 +1,4 @@
+export { default as Animation } from './Animation.js';
+export { default as AnimationFrame } from './AnimationFrame.js';
+export { default as CanvasEngine } from './CanvasEngine.js';
+export { default as Sprite } from './Sprite.js';
