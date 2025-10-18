4a7278132e62c34cd3c8f31c451d347648fa0012
ivanvihe <52743025+ivanvihe@users.noreply.github.com>
2025-10-18 13:02:27 +0200

    Refactor nameplates and chat bubble UI

---
+++ client/src/config/ui.js
@@ -0,0 +1,13 @@
+export const DEFAULT_ENGINE_UI = Object.freeze({
+  nameplate: {
+    offset: { x: 0, y: -4 },
+    heightMultiplier: 1.4
+  },
+  chatBubble: {
+    offset: { x: 16, y: -2 },
+    duration: 2800,
+    fadeDuration: 550
+  }
+});
+
+export default DEFAULT_ENGINE_UI;
